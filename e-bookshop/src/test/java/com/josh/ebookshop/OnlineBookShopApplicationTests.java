d44c67e507ea9944c159e3157576ed9dededaabe
Admin <sweetamca@gmail.com>
2021-12-19 23:53:15 +0400

    Initial commit. Created spring boot backend application and configured the application properties.

---
+++ e-bookshop/src/test/java/com/josh/ebookshop/OnlineBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.ebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
